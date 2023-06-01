1238677aa7b1d5aa671e3272179f4f5a86ea4b4f
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-06-01 05:04:09 +0100

    ignore editor specific directory

--- src/commands/ping.command.ts
+++ src/commands/ping.command.ts
@@ -1,9 +1,13 @@
-import { ApplicationCommand, SlashCommandBuilder } from 'discord.js';
+import { ApplicationCommand, CacheType, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 import type { Command } from '../interface';
 
 export const command: Command = {
   deets: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with pong if Duke is online.')
+    .setDescription('Replies with pong if Duke is online.'),
+
+  execute: async (interaction: ChatInputCommandInteraction<CacheType>) => {
+    await interaction.reply
+  }
 };
